f41d47893726eea077469d344ac736a2a6f8cfd4
ayushisamanta1215 <100126331+ayushisamanta1215@users.noreply.github.com>
2026-05-28 01:10:48 +0530

    Create Student.java

---
+++ Day2/Assignment 1/Student.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private String course;
+    private int marks;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
