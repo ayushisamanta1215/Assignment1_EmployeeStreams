5faef26d02613138835a1e2f22e7d97ebe7ad42b
ayushisamanta1215 <100126331+ayushisamanta1215@users.noreply.github.com>
2026-05-29 00:52:04 +0530

    Update DepartmentController.java

--- Day 3/Assignment 1/DepartmentController.java
+++ Day 3/Assignment 1/DepartmentController.java
@@ -1,1 +1,21 @@
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+    @GetMapping("/{id}")
+    public Map<String, Object> getDepartment(@PathVariable Long id) {
+
+        Map<String, Object> dept = new HashMap<>();
+        dept.put("departmentId", 101);
+        dept.put("departmentName", "Engineering");
+        dept.put("location", "Bangalore");
+        dept.put("managerName", "Anil Kumar");
+
+        return dept;
+    }
+}
